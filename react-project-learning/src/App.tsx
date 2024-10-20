c090a6c880317fe675a682e99629e7afbbf8b35a
Julia Lopes <julialopes@usp.br>
2024-10-19 22:39:42 -0300

    criando pastas

--- react-project-learning/src/App.tsx
+++ react-project-learning/src/App.tsx
@@ -1,15 +1,14 @@
 import { Footer } from "./componentes/Footer"
 import { Navbar } from "./componentes/navbar"
 import { Section } from "./componentes/Section"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    <Navbar />
-     <Section />
-     <Footer />
-    </>
+    <BrowserRouter> 
+    
+    </BrowserRouter>
   )
 }
 
